bb3aa2b0f02918487e660c0183b5f0cac6a13a6b
m4nu-git <manudev912@gmail.com>
2025-08-03 12:23:23 +0530

    initial commit

---
+++ hooks/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+// https://hooks.zapier.com/hooks/catch/80784599/7587b879/
+//passsword logic
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+
+  //store in db a new trigger
+
+  //push it. on to a queue (kafka/redis)
+});
